b612fad0e18d4fb7c5057a1f515be26f036ad6d5
zeasratwde <zeasratwde@live.com>
2015-05-12 22:02:06 -0400

    first commit

---
+++ src/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Net.Mime;
+using System.Threading.Tasks;
+using System.IO;
+using ExtractImage;
+
+
+namespace ExtractImage
+{
+    class Program
+    {
+        static  void  Main(string[] args)
+        {
+
+			WebClient myWebClient = new WebClient();
+
+			string uri = "http://www.statesymbolsusa.org/categories/state-flag";
+			// read the page
+
+			string html = new WebClient().DownloadString(uri);
+			int fileSize = html.Length;
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(uri);
+            // find the image uri
+            int start = html.IndexOf("views-field-field-image",0);    // this is where the div for the flag image beigns
+            int startOfNewTarget,endOfNewTarget;
+			string fileName="", localFileName="";
+
+			LocalFile localFile = new LocalFile(); 
+
+			do
+			{
+				startOfNewTarget = html.IndexOf("src=", start) + 5;	 // skip 'src="'
+				endOfNewTarget = html.IndexOf("?", startOfNewTarget);   // this is the end of the remote fileName
+
+                int remoteFileNameLength = endOfNewTarget - startOfNewTarget;
+                // extract the remote file name 
+                string remoteFileName = html.Substring(startOfNewTarget, remoteFileNameLength);
+                
+                localFileName = localFile.CreateLocalFileName(remoteFileName);
+                Console.WriteLine(localFileName);
+                if (!localFileName.Equals("End"))
+				fileName = @"C:\Users\Asrat\Desktop\StatesData\" + localFileName;
+
+
+				 using  (WebClient client = new WebClient())
+                {
+
+					 if(!File.Exists(fileName))
+                       client.DownloadFile(remoteFileName,fileName);
+                }
+
+                start = endOfNewTarget; // advance the the pointer
+            } while (!localFileName.Equals("WY_flag.jpg"));   // the last state 
+
+
+        }
+
+        
+    
+    }
+}
---
+++ src/ReadStateNamesAndInitials.cs
@@ -0,0 +1,60 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtractImage
+{
+	public class State
+	{
+		public string name { get; set; }
+		public string abbreviation { get; set; }
+	}
+
+	class LocalFile
+	{
+		List<State> listOfStates;
+		public LocalFile()
+		{
+			using (StreamReader j = new StreamReader(@"C:\Users\Asrat\Desktop\StatesData\states_titlecase.json"))
+			{
+				string json = j.ReadToEnd();
+
+				listOfStates = JsonConvert.DeserializeObject<List<State>>(json);
+			}
+		}
+
+
+
+		/// <summary>
+		///  takes a string that is a remote file name and  
+		///  converts it to a string that looks like stateInitial_falg.jpg 
+		///  example 'MA_flag.jpg' at the end of the list it returns 
+		///  the string 'End'
+		/// </summary>
+		/// <param name="str"></param>
+		/// <returns></returns>
+		public string CreateLocalFileName(string rFileName)
+		{
+
+			int size = listOfStates.Count;
+			int count = 0;
+			while (count < size)
+			{
+				if (rFileName.ToUpper().Contains(listOfStates[count].name.ToUpper()))
+				{
+					return listOfStates[count].abbreviation + "_flag.jpg";
+				}
+				count++;
+			}
+			return "End";
+		}
+
+
+
+	}
+}
+
